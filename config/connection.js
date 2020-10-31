9cb50ab5adfbfbcb497109104a7331818e94f0c9
Casey Morgan <caseyjeanmorgan@gmail.com>
2020-10-30 22:12:21 -0500

    updated connection in connection.js

--- config/connection.js
+++ config/connection.js
@@ -15,14 +15,6 @@ if(process.env.JAWSDB_URL) {
   });
 }
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
-// Export connection for our ORM to use.
 module.exports = connection;
