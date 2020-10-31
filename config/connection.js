159394352f4d29d714874200d60259de830bd7f9
Casey Morgan <caseyjeanmorgan@gmail.com>
2020-10-30 21:39:29 -0500

    typo wah-wah

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var mysql = require("mysql");
 var connection;
 
 if(process.env.JAWSDB_URL) {
-  connection = mysqul.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: "localhost",
